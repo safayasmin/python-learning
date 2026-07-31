6c6af4bde95ac03fe0ab89ba60190c8b8121bd9f
safa <safayasmin567@gmail.com>
2026-07-31 12:16:43 +0530

    how calculate a value in python

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-print("helo","world!")
-print("safa yasmin")
-print("hellooooo")
+# print("helo","world!")
+# print("safa yasmin")
+# print("hellooooo")
+# print(35)
 
 
-print(35)
+print(3+3)
+print(10-3)
